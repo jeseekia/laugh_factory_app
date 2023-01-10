8c3b37df1d33e37366f12673329be1023e002dad
Anibal Hugo <ani2bal@yahoo.com>
2023-01-09 20:41:07 -0500

    adding db

--- app.py
+++ app.py
@@ -1,2 +1,13 @@
-from laughfactory import create_app
-app = create_app
+# config
+from flask import Flask
+app = Flask(__name__)
+
+# index route
+@app.route('/')
+def index():
+    return 'Welcome to the LaughFactory'
+
+# meme index route
+@app.route('/memes')
+def memes():
+    return 'These are the memes'
--- laughfactory/__init__.py
+++ laughfactory/__init__.py
@@ -2,6 +2,13 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI']= 'postgresql://postgres:tyler7820@localhost:5432/laugh-factory-app'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from . import models
+    models.db.init_app(app)
+
     @app.route('/')
     def hello():
         return "Hello, it's Meme!"
