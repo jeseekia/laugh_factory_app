71c9310c3cd8136aa8240bdfc268f6cb65b631da
chosenbyme <chosenbyme@hotmail.com>
2023-01-09 19:35:38 -0500

    fixed routes

--- laughfactory/laugh.py
+++ laughfactory/laugh.py
@@ -11,11 +11,11 @@ def index():
     return render_template('index.html',memes=memes)
 
 @bp.route('/login')
-def show_pet():
+def login():
     return render_template('login.html')
 
-@bp.route('/new')
-def new_pet():
+@bp.route('/signup')
+def signup():
     return render_template('signup.html')
 
 memes = json.load(open('meme.json'))
