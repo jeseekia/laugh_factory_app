664e28fb3c22264b9b34aedaaad135990fadea2f
chosenbyme <chosenbyme@hotmail.com>
2023-01-11 20:55:49 -0500

    fixed referencing issue

--- laughfactory/login.py
+++ laughfactory/login.py
@@ -2,6 +2,7 @@ from flask import ( Blueprint, render_template, request, redirect )
 
 bp = Blueprint('login', __name__, url_prefix="/login")
 
-@bp.route('/')
+@bp.route('/',methods=['GET', 'POST'])
 def login():
     return render_template('login.html')
+
