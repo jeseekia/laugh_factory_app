9b6102bacd641b5da4e9321e9fae30d4150fc645
chosenbyme <chosenbyme@hotmail.com>
2023-01-19 13:01:23 -0500

    intergrated up to creating post

--- laughfactory/__init__.py
+++ laughfactory/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 from flask_login import LoginManager
@@ -20,7 +20,6 @@ def create_app():
     app.register_blueprint(auth, url_prefix="/")
 
     from .models import User, Post, Comment, Like
-
 
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
